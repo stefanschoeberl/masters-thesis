146b64043969a0a5708a7c5f3148e6399496dd36
Stefan Schöberl <stefan.schoeberl@icloud.com>
2020-05-11 21:40:40 +0200

    Add Rust

---
+++ src/technischeGrundlagen/rustJS.js
@@ -0,0 +1,4 @@
+import * as wasm from "point-module";
+
+let point = wasm.Point.new(3, 4);
+wasm.alert_point(point);
---
+++ src/technischeGrundlagen/rustLib.rs
@@ -0,0 +1,26 @@
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub struct Point {
+    x: u32,
+    y: u32,
+}
+
+#[wasm_bindgen]
+impl Point {
+    pub fn new(x: u32, y: u32) -> Point {
+        Point { x, y }
+    }
+}
+
+#[wasm_bindgen]
+pub fn alert_point(point: &Point) {
+    alert(&format!("Point({}|{})", point.x, point.y));
+}
