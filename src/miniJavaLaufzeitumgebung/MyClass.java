cd31ed92c4f1a0e9edd002a4b461ba9e534bdfde
Stefan Schöberl <stefan.schoeberl@icloud.com>
2020-05-31 19:34:43 +0200

    Begin MiniJava specification

---
+++ src/miniJavaLaufzeitumgebung/MyClass.java
@@ -0,0 +1,11 @@
+class MyClass {
+    int x;
+    { x = 10; } // Instance Initializer
+
+    static int y;
+    static { y = 100; } // Static Initializer
+
+    public MyClass(int x) { // Constructor
+        this.x = x;
+    }
+}
